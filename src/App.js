c56095b5f87988729f189a2523ec7dbdb0d27b5e
Mohammad <its_mh@outlook.com>
2021-10-13 17:17:48 +0300

    added a spinning logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 /** @format */
 
-import logo from "./logo.svg";
+import logo from "./icons/circle.svg";
 import "./App.css";
 import Nav from "./components/LoadingPage/Nav/Nav";
-
+// import Home from "./components/Main/Home/Home";
+// import Logo2 from "./components/Main/Logo2/Logo2";
 function App() {
 	return (
 		<div className='App'>
+			<img src={logo} alt='MH Logo' className='mhLogo' />
+
 			<div className='loadingPage'>
 				<Nav></Nav>
 			</div>
---
+++ src/components/Main/Home/Home.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+// import styles from "./Home.css";
+import Carousel from "../Projects/Carousel/Carousel";
+const Home = () => {
+	return <div></div>;
+};
+
+export default Home;
